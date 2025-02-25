8a5adf7a4837c97d1367ae0256b794d971e92ecb
XuanHoang1505 <ngokhong15052004@gmail.com>
2025-02-25 20:08:18 +0700

    Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,29 @@
+import classNames from 'classnames/bind';
+import { IoIosCloseCircleOutline, IoMdDownload, IoIosSearch } from "react-icons/io";
+
+import styles from './Header.module.scss'
+import images from '~/assets/images';
+
+const cx = classNames.bind(styles)
 function Header() {
-    return <h2>Header</h2>;
+    return <header className={cx('wrapper')}>
+        <div className={cx('inner')}>
+            <img src={images.logo} alt="Tiktok"/>
+            <div className={cx('search')}>
+                <input placeholder='Search accounts and videos' spellCheck='false'/>
+                <button className={cx('clear')}>
+                    <IoIosCloseCircleOutline />
+                </button>
+                <IoMdDownload className={cx('loading')}/>
+                <button className={cx('search-btn')}>
+                    <IoIosSearch />
+                </button>
+            </div>
+            <div className={cx('action')}>
+
+            </div>
+        </div>
+    </header>
 }
 
 export default Header;
